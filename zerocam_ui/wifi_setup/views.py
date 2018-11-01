7741a9dad62963cf4ebe222b046a0802e86760ec
matdoess <matthias.doess@gmail.com>
2018-11-01 15:14:11 +0100

    added script to change ap mode

--- zerocam_ui/wifi_setup/views.py
+++ zerocam_ui/wifi_setup/views.py
@@ -5,6 +5,10 @@
 from django.http import HttpResponse
 from django.template import loader
 
+import shlex
+from subprocess import Popen
+from subprocess import run
+from time import sleep
 
 def index(request):
     template = loader.get_template('index.html')
@@ -21,5 +25,8 @@ def form_submit(request):
         'ssid': request.POST['ssid'],
         'password': request.POST['password'],
     }
+
+    pscmd = shlex.split("sudo /home/pi/zerocam/script/switchap.sh wifi 'FRITZ!Box 7590 SR' 25902909888682070047")
+    run(pscmd)
 
     return HttpResponse(template.render(context, request))
