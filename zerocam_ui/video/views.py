686aca45865070058780f38bb7060f14cda954b8
matdoess <matthias.doess@gmail.com>
2018-11-05 22:26:23 +0100

    picam.service button

--- zerocam_ui/video/views.py
+++ zerocam_ui/video/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+import shlex
+from subprocess import Popen
+from subprocess import run
+
 def index(request):
     template = loader.get_template('video-index.html')
 
@@ -13,7 +17,17 @@ def index(request):
 def live(request):
     template = loader.get_template('video-live.html')
 
+    btnpicamstate = request.GET.get("btnpicamstate", "")
+
     context = {
+        "btnpicamstate" : btnpicamstate
     }
     
+    if btnpicamstate == 'on':
+        pscmd = shlex.split("sudo systemctl start picam.service")
+        run(pscmd)
+    if btnpicamstate == 'off':
+        pscmd = shlex.split("sudo systemctl stop picam.service")
+        run(pscmd)
+
     return HttpResponse(template.render(context, request))
